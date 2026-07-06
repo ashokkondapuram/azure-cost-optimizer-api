3ae343a5139e0f156af9bcca26973d241b3543f6
ashokkondapuram <ashok.k14@outlook.com>
2026-07-05 21:03:34 -0400

    feat(ui): import hub-ui-improvements.css for all added-feature polish

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import './styles/print.css';
+import './styles/hub-ui-improvements.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
